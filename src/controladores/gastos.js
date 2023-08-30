1a9d901d47d7a9fcf395e353cc0a7a8476e93fed
Lucas Meira <lucasmeirasouza@hotmail.com>
2023-08-30 12:56:46 -0300

    controlador Listar Gasto criado para detalhar o gasto de um usuario

--- src/controladores/gastos.js
+++ src/controladores/gastos.js
@@ -5,23 +5,53 @@ const listarGastos = (req, res) => {
     const { id } = req.params;
 
     if (isNaN(Number(id))) {
-        return res.json({ mensagem: "O id deve ser numérico." });
+        return res.status(400).json({ mensagem: "O id deve ser numérico." });
     };
 
     const pessoaEncontrada = encontrarUsuarioPeloId(id);
 
     if (!pessoaEncontrada) {
-        return res.json({ mensagem: "Usuario não encontrado." });
+        return res.status(404).json({ mensagem: "Usuario não encontrado." });
     };
 
     const gastosEncontrados = encontrarGastosPeloId(id);
 
     if (gastosEncontrados.length === 0) {
-        return res.json({ mensagem: "Esse usuario não teve gastos." });
+        return res.status(400).json({ mensagem: "Esse usuario não teve gastos." });
     };
-    return res.json(gastosEncontrados);
+    return res.status(200).json(gastosEncontrados);
+};
+
+
+const listarGasto = (req, res) => {
+    const { idPessoa, idGasto } = req.params;
+    if (isNaN(Number(idPessoa)) || isNaN(Number(idGasto))) {
+        return res.status(400).json({ mensagem: "O id deve ser numérico." });
+    };
+
+    const pessoaEncontrada = encontrarUsuarioPeloId(idPessoa);
+    if (!pessoaEncontrada) {
+        return res.status(404).json({ mensagem: "Usuario não encontrado." });
+    };
+
+    const gastosEncontrados = encontrarGastosPeloId(idPessoa);
+    if (gastosEncontrados.length === 0) {
+        return res.status(404).json({ mensagem: "Gasto não encontrado." });
+    };
+
+    const gastoEncontrado = gastosEncontrados.find((gasto) => {
+        return gasto.id === Number(idGasto)
+    });
+
+    if (!gastoEncontrado) {
+        return res.status(404).json({ mensagem: "Gasto não encontrado." });
+    }
+
+    return res.json(gastoEncontrado)
+
 };
 
 module.exports = {
-    listarGastos
+    listarGastos,
+    listarGasto
 }
--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { listarGastos } = require('./controladores/gastos');
+const { listarGastos, listarGasto } = require('./controladores/gastos');
 
 const rotas = express();
 
 rotas.get('/usuarios/:id/gastos', listarGastos);
-
+rotas.get('/usuarios/:idPessoa/gastos/:idGasto', listarGasto)
 module.exports = rotas;
